be69db660c4a51aae30894656b699e55caf60661
MananDesai54 <manan5401desai@gmail.com>
2021-04-23 19:09:03 +0530

    more on animation

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { motion } from "framer-motion";
 import React from "react";
 import "./App.css";
 import Splash from "./Components/Splash/Splash";
@@ -5,7 +6,9 @@ import Splash from "./Components/Splash/Splash";
 function App() {
   return (
     <div className="App">
-      <Splash />
+      <motion.div>
+        <Splash />
+      </motion.div>
     </div>
   );
 }
