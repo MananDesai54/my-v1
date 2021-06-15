6e0b695dc6acd4fb883419499e845bdbf31fc2e9
MananDesai54 <manan5401desai@gmail.com>
2021-06-15 21:48:36 +0530

    updayte

--- src/index.js
+++ src/index.js
@@ -4,13 +4,13 @@ import "./tailwind.css";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter basename="/my-v1">
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>,
   document.getElementById("root")
 );
